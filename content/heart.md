f5e6d3d3626a6460681dc6aaf0af39e72696fbc0
whiler <wenwu500@qq.com>
2018-03-29 18:41:03 +0800

    use pelican

---
+++ content/heart.md
@@ -0,0 +1,51 @@
+UUID: 57615493-5228-472d-9945-ac0dcd0d4c00
+Title: 绘制心形图案
+Summary: 半夜没事瞎折腾，想到绘制心形图案消磨时间。
+Date: 2016-04-14
+
+半夜没事瞎折腾，想到绘制心形图案消磨时间。
+看了一些方案，最后采用半圆加正方形折叠的方式来绘制，`r = a * (1 - sin θ)` 真心不会。
+
+绘制心形图案，首先想到了笛卡尔的 `r = a * (1 - sin θ)` ，但是折腾了半天不会将极坐标系的点转换到笛卡尔坐标系，于是改用正方形外接一个半圆再折叠一次来表示。
+这样就只剩下简单的几何代数运算了。
+
+代码：
+```
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+from math import sin, sqrt, pi
+
+
+def heart(radius):
+    side = radius * sin(pi / 4.0)
+    offset = radius + side
+    steps = radius + 3.0 * side
+    top = 0
+    while top <= steps:
+        if top < offset:
+            delta = sqrt(top * (2.0 * radius - top))
+            left_min = side - delta
+            if left_min < 0:
+                left_min = 0.0
+            left_max = side + delta
+        else:
+            left_max = steps - top
+            left_min = 0.0
+
+        line = ''
+        i = 0
+        while i < offset + left_max:
+            if offset - left_max <= i <= offset - left_min:
+                line = line + '*'
+            elif offset + left_min <= i <= offset + left_max:
+                line = line + '*'
+            else:
+                line = line + ' '
+            i = i + 1
+
+        print(line)
+
+        top = top + 1
+```
---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'whiler'
+SITENAME = '轨迹/trace'
+SITESUBTITLE = 'I leave no trace of wings in the air, but I am glad I have had my flight.'
+SITEURL = 'http://local.whiler.me:8000'
+
+PATH = 'content'
+THEME = 'theme'
+
+TIMEZONE = 'Asia/Shanghai'
+
+# git clone https://github.com/getpelican/pelican-plugins
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS=['sitemap']
+
+DEFAULT_LANG = 'cmn'
+AUTHOR_SAVE_AS = False
+CATEGORY_SAVE_AS = False
+TAG_SAVE_AS = False
+ARCHIVES_SAVE_AS = 'archives.html'
+DIRECT_TEMPLATES = ('index', 'archives')
+ARTICLE_EXCLUDES = ['webroot']
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feed.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+TYPOGRIFY = None
+
+STATIC_PATHS = ['assets', 'webroot/.nojekyll', 'webroot/LICENSE', 'webroot/README.md', 'webroot/robots.txt']
+EXTRA_PATH_METADATA = {
+    'webroot/.nojekyll': {'path': '.nojekyll'},
+    'webroot/LICENSE': {'path': 'LICENSE'},
+    'webroot/README.md': {'path': 'README.md'},
+    'webroot/robots.txt': {'path': 'robots.txt'}
+}
+
+DISQUS_SITENAME = 'traced'
+GOOGLE_ANALYTICS = 'UA-74531835-1'
+
+DEFAULT_PAGINATION = 7
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'weekly'
+    }
+}
+
+RELATIVE_URLS = True

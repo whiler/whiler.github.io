8994927243dc038984d4a2a84d94852201c5f8da
whiler <wenwu500@qq.com>
2018-04-04 02:02:59 +0800

    Generate Pelican site

--- theme/scripts/mathjax.js
+++ theme/scripts/mathjax.js
@@ -1,6 +1,6 @@
 (function(win, doc) {
-	if (0 < doc.querySelectorAll('script[type^="math/tex"]').length) {
-		win.taskQueue.enqueue('create', 'script', {src: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/latest.js?config=TeX-MML-AM_CHTML'});
-	}
-	return true;
+    if (0 < doc.querySelectorAll('script[type^="math/tex"]').length) {
+        win.taskQueue.enqueue('create', 'script', {src: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/latest.js?config=TeX-MML-AM_CHTML'});
+    }
+    return true;
 })(window, document);

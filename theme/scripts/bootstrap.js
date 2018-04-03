29fe096020063072425058c741f9d58a81704110
whiler <wenwu500@qq.com>
2018-04-03 16:27:41 +0800

    Generate Pelican site

--- theme/scripts/bootstrap.js
+++ theme/scripts/bootstrap.js
@@ -1,6 +1,15 @@
 (function(win, doc) {
-	function create(args) {
-	}
+    function getQueryVariable(name) {
+		var query = win.location.search.substring(1),
+		    vars = query.split('&');
+    	for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) == name) {
+				return decodeURIComponent(pair[1]);
+			}
+    	}
+		return null;
+    }
 
 	function handleTask(args) {
 		var action = args[0];
@@ -10,30 +19,36 @@
 				element.setAttribute(name, attrs[name]);
 			}
 			(doc.head || doc.body).appendChild(element);
+			delete element;
 		} else if ('wait' == action) {
 			if (args[1]()) {
 				args[2]();
 			} else {
 				taskQueue.enqueue('wait', args[1], args[2]);
 			}
+		} else {
+			console.warn(args);
+			return false;
 		}
-		return console.info(args);
+		return true;
 	}
 
-	if ('undefined' != typeof(win.Worker) && 'undefined' != typeof(doc.querySelectorAll)) {
-		var times = 0, max = 200, sleep = 300;
+	if ('undefined' != typeof(win.Worker) && 'undefined' != typeof(doc.querySelectorAll) && !getQueryVariable('noscript')) {
+		var times = 0, max = 200, sleep = 300, domReady = false;
 		var consumer = function() {
 			times++;
-			console.debug('consumer checking');
 			var taskLength = taskQueue.getLength();
-			if (doc.readyState === 'complete' && 0 < taskLength) {
-				console.debug(taskQueue.getLength() + ' task(s) in taskQueue');
+			domReady = domReady || doc.readyState === 'complete';
+			if (domReady && 0 < taskLength) {
+				console.info(taskQueue.getLength() + ' task(s) in taskQueue');
 				for (var i = 0; i < taskLength; i++) {
 					handleTask(taskQueue.dequeue());
 				}
 			}
 			if (times < max) {
 				setTimeout(consumer, sleep);
+			} else {
+				console.warn('ignore any task');
 			}
 			return true;
 		};

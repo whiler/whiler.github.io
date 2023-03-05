7377d97ef9bfe60aca312a03d1a84d5ecec868da
whiler <wenwu500@qq.com>
2023-03-05 09:25:32 +0800

    content only

--- theme/static/scripts/mathjax.js
+++
@@ -1,9 +0,0 @@
-(function(win, doc) {
-    var nodes = doc.querySelectorAll('.arithmatex');
-    if (0 < nodes.length) {
-        win.taskQueue.enqueue('create', 'script', {src: '//polyfill.io/v3/polyfill.min.js?features=es6'});
-        win.taskQueue.enqueue('create', 'script', {src: '//cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js', id: 'MathJax-script', async: true});
-    }
-    delete nodes;
-    return true;
-})(window, document);
